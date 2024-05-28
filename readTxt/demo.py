238b61f906ed6b24a125e24e84bfc65ddb1bd6af
周圣道 <zsdnishishui@163.com>
2024-05-28 09:58:07 +0800

    查找文本中最长的那一行

--- readTxt/demo.py
+++ readTxt/demo.py
@@ -1,3 +1,12 @@
+# 查找最长的哪一行数据
+length_arr = []
+max_line = 0 # 最长行数
+max_length = 0 # 最大长度
+max_line_txt = '' # 最长行
 with open('test.sql', 'r', encoding='utf-8') as f:
-    for line in f.readlines():
-        print(line)
+    for i,line in enumerate(f.readlines()):
+        if len(line) > max_length:
+            max_length = len(line)
+            max_line = i+1
+            max_line_txt = line
+print(max_line,max_length,max_line_txt)
