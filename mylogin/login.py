5fcbf4ce69f4b89165e7e102c57cc103a380e0e4
周圣道 <zsdnishishui@163.com>
2024-05-29 17:49:18 +0800

    保存上次登陆成功的用户名密码

--- mylogin/login.py
+++ mylogin/login.py
@@ -8,14 +8,14 @@ import execjs
 from crypto import ctx
 
 def login(username, password, encryption=True):
-    key = execjs.compile(requests.get('******').text)
+    key = execjs.compile(requests.get('****').text)
     encrypt_config_key = key.eval('encrypt_config_key')
     encrypt_config_iv = key.eval('encrypt_config_iv')
     if encryption:
         username = ctx.call('stringtoaesencrypt', username, encrypt_config_key, encrypt_config_iv)
         password = ctx.call('stringtoaesencrypt', password, encrypt_config_key, encrypt_config_iv)
     # 返回code
-    code = requests.post('****', {'submit': 'submit'}).json()['code']
+    code = requests.post('***', {'submit': 'submit'}).json()['code']
 
     # 登录
     data = {'username': username,
@@ -25,7 +25,7 @@ def login(username, password, encryption=True):
             'code': code,
             'submit': 'submit'}
     # print(data)
-    r = requests.post('****', data)
+    r = requests.post('***', data)
     r.encoding = r.apparent_encoding
     if not r.text.startswith('0#'):
         with open('data.txt', 'w') as file:
@@ -37,9 +37,17 @@ def login(username, password, encryption=True):
 
 
 def main():
+    # 查询保存的用户名密码，如果没有的话让用户自己输入
+    username, password = get_save()
+    if username == '' or password == '':
+        username = input("请输入用户名：")
+        password = input("请输入密码：")
+        print(login(username, password))
+    else:
+        print(login(username, password, False))
+def get_save():
     username = ''
     password = ''
-    # 查询保存的用户名密码，如果没有的话让用户自己输入
     try:
         with open('data.txt', 'r') as f:
             for i, line in enumerate(f.readlines()):
@@ -49,14 +57,7 @@ def main():
                     password = line.strip()
     except FileNotFoundError:
         print("没有找到用户名密码，请手动输入")
-
-    if username == '' or password == '':
-        username = input("请输入用户名：")
-        password = input("请输入密码：")
-        print(login(username, password))
-    else:
-        print(login(username, password, False))
-
+    return username, password
 
 if __name__ == '__main__':
     main()
--- mylogin/qt.py
+++ mylogin/qt.py
@@ -1,8 +1,8 @@
-
 import sys
 from PyQt6.QtWidgets import (QWidget, QLabel, QLineEdit,
                              QTextEdit, QGridLayout, QApplication, QPushButton)
-from login import login
+from login import login, get_save
+
 
 class Example(QWidget):
 
@@ -18,39 +18,43 @@ class Example(QWidget):
 
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
     def initUI(self):
-
-        title = QLabel('用户名：')
-        author = QLabel('密码：')
-        btn = QPushButton('登陆', self)
-
-        # btn.clicked.connect(None)
-        titleEdit = QLineEdit()
-        authorEdit = QLineEdit()
+        username, password = get_save()
         reviewEdit = QTextEdit()
         grid = QGridLayout()
         grid.setSpacing(10)
+        if username == '' or password == '':
+            title = QLabel('用户名：')
+            author = QLabel('密码：')
+            titleEdit = QLineEdit()
+            authorEdit = QLineEdit()
+            btn = QPushButton('登陆', self)
+            grid.addWidget(title, 1, 0)
+            grid.addWidget(titleEdit, 1, 1)
 
-        grid.addWidget(title, 1, 0)
-        grid.addWidget(titleEdit, 1, 1)
+            grid.addWidget(author, 2, 0)
+            grid.addWidget(authorEdit, 2, 1)
 
-        grid.addWidget(author, 2, 0)
-        grid.addWidget(authorEdit, 2, 1)
+            grid.addWidget(btn, 3, 0)
+            btn.clicked.connect(lambda: self.clickButton(titleEdit.text(), authorEdit.text(), True, reviewEdit))
+        else:
+            self.clickButton(username, password, False, reviewEdit)
 
-        grid.addWidget(btn, 3, 0)
         grid.addWidget(reviewEdit, 3, 1, 5, 1)
-        btn.clicked.connect(lambda: self.clickButton(titleEdit.text(), authorEdit.text(),reviewEdit))
+
         self.setLayout(grid)
 
         self.center()
         self.setWindowTitle('自动登陆')
         self.show()
-    def clickButton(self, username, password, reviewEdit=None):
-        res = login(username, password)
+
+    def clickButton(self, username, password, encryption, reviewEdit=None):
+        res = login(username, password, encryption)
         reviewEdit.setText(res)
 
-def main():
 
+def main():
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec())
