cd53581836307c2f5d53832dbb0906ef6e217950
zsdnishishui <zsdnishishui@163.com>
2024-06-10 21:58:32 +0800

    小草社区图片下载

--- dowload/imgDownLoad.py
+++ dowload/imgDownLoad.py
@@ -1,13 +1,13 @@
 import os
+import random
 import threading
-
 import requests
 from bs4 import BeautifulSoup
+from headers import headers_list
 
-headers ={
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-}
-html = requests.get("https://cl.3525x.xyz/htm_data/2406/7/6360416.html",headers=headers).text
+headers = random.choice(headers_list)
+htmlUrl = 'https://cl.3525x.xyz/htm_data/2406/16/6333339.html'
+html = requests.get(htmlUrl, headers=random.choice(headers_list)).text
 # 创建Beautiful Soup对象
 soup = BeautifulSoup(html, 'html.parser')
 list = []
@@ -18,23 +18,30 @@ for i in img:
     if i.has_attr('ess-data'):
         list.append(i['ess-data'])
         # print(i['ess-data'])
-dir = 'd:/Temp/img/'+str(name);
+dir = 'd:/Temp/img/' + str(name);
 if not os.path.exists(dir):
     os.makedirs(dir)
 
 print("总共 %d 张照片" % (len(list)))
 
-def downLoad(i,url,dir):
-    print(url)
+
+def downLoad(i, url, dir):
+    # print(url)
     file = url.split("/")[-1]
-    r = requests.get(url, headers=headers)
+    try:
+        r = requests.get(url, headers=random.choice(headers_list))
+    except:
+        for j in range(4):  # 循环去请求网站
+            r = requests.get(url, headers=random.choice(headers_list))
+            if r.status_code == 200:
+                break
+
     # 下载图片
     with open(dir + '/' + file, mode="wb") as f:
         f.write(r.content)  # 图片内容写入文件
         print('已下载第 %d 张' % (i + 1))
 
 
-for i,url in enumerate(list):
-    threading.Thread(target=downLoad, args=(i,url,dir)).start()
+for i, url in enumerate(list):
+    threading.Thread(target=downLoad, args=(i, url, dir)).start()
     # downLoad(i,url,dir)
-
